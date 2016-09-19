cdbe31f9c2a22c2d89c9ee72e618d568b59c8943
Tomohiro SHIOYA <shioyang4321@gmail.com>
2016-09-19 21:51:43 +0900

    Create model/form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+import { FormComponent } from './model/form.component';
+
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/model/form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import '../../../public/css/styles.css';
+
+@Component({
+  selector: 'my-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent { }
